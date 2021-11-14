e4bd608c105bd2250d110025b2cb76ca9bb41538
Hopac1 <chris1101@hotmail.co.uk>
2021-11-14 13:52:18 +0000

    display player sign and switch current player on click consecutively

---
+++ script.js
@@ -0,0 +1,60 @@
+// game board
+// two players
+
+
+// players to be stored in objects
+ // this should be a factory function
+ const Player = function(name, sign) {
+    const playerInfo = {name: name, sign: sign};
+    return playerInfo;
+ }
+
+// object to control the flow of the game itself
+const displayController = function() {
+    console.log("blank");
+}
+
+// store gameboard as array inside Gameboard object
+const gameBoard = (function() {
+    const boardCells = document.querySelectorAll(".cell");
+    const gameGrid = document.querySelector(".game-grid");
+    let boardArray = ["", "", "",
+                     "", "", "",
+                     "", "", "",];
+
+    const playerOne = Player("playerOne", "X");
+    const playerTwo = Player("playerTwo", "O");
+    let currentPlayer = playerOne;
+    
+    // Listen for user click
+    boardCells.forEach(cell => cell.addEventListener("click", storeSelection));
+
+    function storeSelection(event) {
+        // Get index of clicked cell, need to add X/O to boardArray, then render() 
+        let index = Array.from(event.target.parentElement.children).indexOf(event.target);
+        console.log(index); // FOR DEBUGGING - TO BE REMOVED
+        console.log(boardArray) // FOR DEBUGGING - TO BE REMOVED
+        boardArray[index] = currentPlayer.sign; // Replace "X" with current player (O or X)
+        changeTurn(currentPlayer)// change currentPlayer to other player
+        console.log(boardArray) // FOR DEBUGGING - TO BE REMOVED
+        _render();
+    }
+
+    function _render() {
+        // render boardArray data to DOM
+        for (let i = 0; i < boardArray.length; i++) {
+            gameGrid.children[i].textContent = boardArray[i];
+        }
+    }
+
+    function changeTurn(player) {
+        if (player.sign === "X") {
+            return currentPlayer = playerTwo;
+        } 
+        else if (player.sign === "O") {
+            return currentPlayer = playerOne;
+        }
+    }
+    return storeSelection, changeTurn
+})();
+

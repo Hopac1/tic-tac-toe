2a481243cafd09ecd61760054c45643db787b3f6
Hopac1 <chris1101@hotmail.co.uk>
2021-11-18 00:27:27 +0000

    add score to player that won, create event listener for restart button

--- script.js
+++ script.js
@@ -2,12 +2,7 @@
 const Player = function(name, sign) {
     const playerInfo = {name: name, sign: sign};
     return playerInfo;
- }
-
-// object to control the flow of the game itself
-const displayController = function() {
-    console.log("blank");
-}
+ };
 
 // store gameboard as array inside Gameboard object
 const gameBoard = (function() {
@@ -18,13 +13,20 @@ const gameBoard = (function() {
                      "", "", "",];
 
     const playerOne = Player("playerOne", "X");
-    let playerOneArr = [];
     const playerTwo = Player("playerTwo", "O");
-    let playerTwoArr = [];
     let currentPlayer = playerOne;
+
+    const winnerSpan = document.querySelector(".round-winner");
+    const playerOneSpan = document.querySelector(".player-one");
+    const playerTwoSpan = document.querySelector(".player-two");
+    let playerOneScore = 0;
+    let playerTwoScore = 0;
+
+    const restartButton = document.querySelector(".restart-btn");
     
     // Listen for user click
     boardCells.forEach(cell => cell.addEventListener("click", storeSelection));
+    restartButton.addEventListener("click", clearBoard);
 
     function storeSelection(event) {
         // Get index of clicked cell, need to add X/O to boardArray, then render() 
@@ -35,44 +37,45 @@ const gameBoard = (function() {
             boardArray[index] = currentPlayer.sign; // Replace "X" with current player (O or X)
             _render();
             let isWinner = checkForWin(index);
-            console.log(isWinner) // FOR DEBUGGING - TO BE REMOVED
-            changeTurn(currentPlayer) // change currentPlayer to other player
+            addScore(isWinner);
+            console.log(isWinner); // FOR DEBUGGING - TO BE REMOVED
+            changeTurn(currentPlayer); // change currentPlayer to other player
             console.log(boardArray) // FOR DEBUGGING - TO BE REMOVED
         } else {
             return;
-        }
-    }
+        };
+    };
 
     function _render() {
         // render boardArray data to DOM
         for (let i = 0; i < boardArray.length; i++) {
             gameGrid.children[i].textContent = boardArray[i];
-        }
-    }
+        };
+    };
 
     function changeTurn(player) {
         if (player.sign === "X") {
             return currentPlayer = playerTwo;
-        } 
+        }
         else if (player.sign === "O") {
             return currentPlayer = playerOne;
-        }
-    }
+        };
+    };
 
     function isCellAlreadyClicked(cellIndex) {
         if (boardArray[cellIndex] === "") {
             return true;
         } else {
             return false;
-        }
-    }
+        };
+    };
 
     function clearBoard() {
         boardArray = ["", "", "",
                      "", "", "",
                      "", "", "",];
         _render();
-    }
+    };
 
     function checkForWin() {
         const winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
@@ -85,18 +88,34 @@ const gameBoard = (function() {
                     return boardArray[threeMarks] === aSign;
                 });
             });
-        }
-        // some(), every()
-        //Iterate over winConditions array, each element is a single win condition.
+        };
         const didXWin = winFactory("X");
-
         const didOWin = winFactory("O");
 
-        return `X: ${didXWin} | O:${didOWin}`;
-        // For each win condition, check if each value is present in boardArray
-        // for X and O.
-        // some() on winConditions array, every() for each element inside each array element
-    }
+        if (didXWin === true) {
+            return "x";
+        } else if (didOWin === true) {
+            return "o";
+        } else {
+            return false;
+        }
+    };
+
+    function addScore(player) {
+        if (player === false) {
+            return;
+        } else if (player === "x") {
+            playerOneScore += 1;
+            playerOneSpan.textContent = playerOneScore;
+            winnerSpan.textContent = "Player One Wins!";
+
+        } else if (player === "o") {
+            playerTwoScore += 1;
+            playerTwoSpan.textContent = playerTwoScore;
+            winnerSpan.textContent = "Player Two Wins!";
+        }
+    };
+
     return {storeSelection, changeTurn, clearBoard};
 })();
 
